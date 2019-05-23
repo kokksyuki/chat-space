f54d07c98e84775a5c1b8c2236f3dc0263780dfe
kokksyuki <kokkayuki10@gmail.com>
2019-05-24 06:46:14 +0900

    update migration file
    change image caram's code

--- db/migrate/20190518073016_create_messages.rb
+++ db/migrate/20190518073016_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :body , null: false
-      t.integer :image, null: false
+      t.integer :image
       t.references :group, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
